1559689f12116ae717ea395e2cd0e19956724381
nimesh-govind <nimeshgovind74@gmail.com>
2020-09-24 18:23:04 +1200

    Install SASS and Bulma

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React from "react";
+import { Route } from "react-router-dom";
 
-import Nav from './Nav'
-import Recipes from './Recipes'
-import Week from './Week'
-import Shopping from './Shopping'
+import "./App.scss";
 
-function App () {
+import Nav from "./Nav";
+import Recipes from "./Recipes";
+import Week from "./Week";
+import Shopping from "./Shopping";
+
+function App() {
   return (
     <div className="App">
       <h1>RECIPLAN APP!</h1>
-      <div className='routes'>
-        <Route path='/' component={Nav} />
-        <Route exact path='/recipes' component={Recipes} />
-        <Route exact path='/week' component={Week} />
-        <Route exact path='/shopping' component={Shopping} />
+      <div className="routes">
+        <a href="#" className="button is-primary">
+          Press Me!
+        </a>
+        <Route path="/" component={Nav} />
+        <Route exact path="/recipes" component={Recipes} />
+        <Route exact path="/week" component={Week} />
+        <Route exact path="/shopping" component={Shopping} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
