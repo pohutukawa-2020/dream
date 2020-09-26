0e0bb63f4fddc77579b9de93b762b544efe9bb2c
Matt Silver <matt@Matts-MacBook-Pro.local>
2020-09-26 15:29:22 +1200

    Delete old weekList, rename weekListMatt

--- src/components/Week.jsx
+++ src/components/Week.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
 import WeekList from './WeekList'
-import WeekListMatt from './WeekListMatt'
 
 function Week () {
   return (
     <div className="Week">
       <h1>Week</h1>
       <WeekList />
-      <WeekListMatt />
     </div>
   )
 }
--- src/components/WeekList.jsx
+++ src/components/WeekList.jsx
@@ -4,41 +4,49 @@ import firebase from 'firebase'
 
 import WeekListItem from './WeekListItem'
 
-function useWeekDays() {
-  const [weekDays, setWeekDays] = useState([])
+function WeekList() {
+  const [week, setWeek] = useState({
+    monday: '',
+    tuesday: '',
+    wednesday: '',
+    thursday: '',
+    friday: '',
+    saturday: '',
+    sunday: ''
+  })
 
   useEffect(() => {
     firebase
       .firestore()
-      .collection('weekDays')
+      .collection('week')
       .onSnapshot(snapshot => {
-        const newWeekDays = snapshot.docs.map(doc => ({
+        const newWeekPlan = snapshot.docs.map(doc => ({
           id: doc.id,
           ...doc.data()
         }))
-        setWeekDays(newWeekDays)
+        setWeek(newWeekPlan[0])
       })
   },[])
-
-  return weekDays
-}
-
-
-function WeekList() {
-  const weekDays = useWeekDays()
 
   return (
     <div className="WeekList">
       <div>
-      <h2>List of days and meals</h2>
-      {weekDays.map(weekDay => 
-        <p key={weekDay.id}>
-          <WeekListItem weekDay={weekDay} />
-        </p>
-      )}
-
+        <h2>List of days and meals</h2>
+        <h3>Monday</h3>
+        {week.monday}
+        <h3>Tuesday</h3>
+        {week.tuesday}
+        <h3>Wednesday</h3>
+        {week.wednesday}
+        <h3>Thursday</h3>
+        {week.thursday}
+        <h3>Friday</h3>
+        {week.friday}
+        <h3>Saturday</h3>
+        {week.saturday}
+        <h3>Sunday</h3>
+        {week.sunday}
       </div>
-
     </div>
   )
 }
