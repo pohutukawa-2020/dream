4c247c1baec6ad4e3eab638a09d94093a9225b4a
Matt Silver <matt@Matts-MacBook-Pro.local>
2020-09-26 17:22:45 +1200

    add button to week view to Add Recipe

--- src/components/WeekList.jsx
+++ src/components/WeekList.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react'
-
+import { Link } from "react-router-dom"
 import { WeekContext } from './WeekContext'
 import { RecipeContext } from './RecipeContext'
 import RecipeCard from './RecipeCard'
+import { Router } from 'react-router-dom'
 
 function WeekList() {
   const [week] = useContext(WeekContext)
@@ -16,9 +17,8 @@ function WeekList() {
         <h2>List of days and meals</h2>
         <h3>Monday</h3>
         {mondayRecipe ? <RecipeCard recipe={mondayRecipe} /> : null}
-        {/* <RecipeCard recipe={mondayRecipe} /> */}
         <h3>Tuesday</h3>
-        {week.tuesday}
+        <Link to='/recipes'><button>Add Recipe For Tuesday</button></Link>
         <h3>Wednesday</h3>
         {week.wednesday}
         <h3>Thursday</h3>
