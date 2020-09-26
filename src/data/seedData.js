42cbb48ce9699f79ce545fbba9aad23fcbfc56d9
michael-rua <michaelrua1@gmail.com>
2020-09-26 21:37:19 +1200

    add recipe

--- src/data/seedData.js
+++ src/data/seedData.js
@@ -50,6 +50,32 @@ const seedRecipes = [
       'Bake for 30 minutes, or till golden.'
     ],
     imagePath: 'https://img.jamieoliver.com/jamieoliver/recipe-database/2ipm2HQHql2Azq2dLfiA4k.jpg?tr=w-800,h-1066'
+  },
+  {
+    name: 'Roast Chicken With Couscous',
+    serves: 'Serves Aliens',
+    prepTime: '90 Minutes',
+    ingredients: [
+      '1.6 kilograms chicken ',
+      '1 lemon',
+      '2 red onions',
+      '2 carrots',
+      '3 sprigs fresh mint leaves',
+      'olive oil',
+      '1 teaspoon ground cumin',
+      '200 grams roasted red peppers, drained',
+      '1 teaspoon smoked paprika',
+      '1 tablespoon ground coriander',
+      '250 grams couscous',
+      'extra-virgin olive oil'
+    ],
+    method: [
+      '1. It’s best to use a nice free-range or organic chicken for this dish, or the best-quality bird you can afford; and I love the idea of the tasty vegetables in this recipe getting a whole lot of love. If you like, a mint sauce would make a great accompaniment. Pretty delish!','2. Preheat the oven to 200°C. Place the chicken on a board and make some deep crisscross slashes into the legs – this will help the spices to penetrate the meat, and will also help it to cook more quickly. Halve the lemon and place inside the chicken, then rub the chicken skin all over with olive oil, a little sea salt, pepper and the cumin. Transfer the chicken to a roasting tray and place in the hot oven.',
+      '3. Peel and quarter the onions and carrots. When the chicken’s been cooking for around 15 minutes, reduce the heat to 180°C and add the onions and carrots to the tray. Roast for a further hour, or until golden and cooked through – the chicken is cooked when the thigh meat pulls easily away from the bone and the juices run clear.',
+      '4. Once perfectly cooked, carefully lift the chicken onto a plate or board and cover with tin foil and a couple of tea towels to keep warm. Spoon the vegetables onto a board and roughly chop them, then tip back into the tray and place on a medium heat. Pour in 500ml boiling water and stir well, making sure you scrape up all the lovely sticky goodness from the bottom of the tray.',
+      '5. Drain and roughly chop the peppers, then add them to the tray with the smoked paprika, ground coriander and the couscous. Pull the lemon halves out of the chicken with a pair of tongs and squeeze the juices into the tray (making sure to catch any pips). Bring to the boil, then turn the heat off and leave to rest for 5 minutes, or until the couscous has absorbed all of the water. Meanwhile, pick and finely chop the mint leaves. When the time’s up, fluff up the couscous using a fork and stir through the chopped mint. Drizzle with a good lug of extra virgin olive oil and everything together.', '6. Cut the chicken up into joints, removing the skin if you want to keep it healthy, then serve with the tasty roast-vegetable couscous. Enjoy!', '7. Nims Tip: Basting a chicken as it cooks helps to stop it drying out, giving you lovely, juicy meat. Check on the chicken every 20 to 30 minutes, and spoon any juices from the bottom of the tray back over the bird.'
+    ],
+    imagePath: 'https://www.flickr.com/photos/tedesco57/9417476876/in/photolist-fmc2LY-bKxwF-cHGzby-sCcAe-d65j1j-8CPk1c-3a7y4r-JHtqxQ-8CVp8Z-d4Zn4-hY254Q-hY1X2W-hY1rpD-hY2fqw-hY1F2h-hY2jXh-aUDzNF-hY1Dmz-hY2c7N-b145ig-5oWRvQ-9cv598-hY2YU8-9PWYLN-JxBZ1V-5JsHVz-4VmcbF-5PcoMY-4sQCTx-5dFB1r-aZWfSH-9d8tSi-rfs1DG-83zUba-WrJjEx-8Nk91Z-662SmA-4x9MYs-gczuB-4hRgca-8NjJSM-6CyLBR-7tPaJL-7BoxHL-21a9HQJ-acnihX-73T8dc-cF8MVA-7uyraS-7uuyVF'
   }
 ]
 
