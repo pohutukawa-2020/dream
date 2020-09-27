127429b577061c52295a063e82677a1158715254
nimesh-govind <nimeshgovind74@gmail.com>
2020-09-27 19:42:20 +1300

    style week planner

--- src/components/WeekList.jsx
+++ src/components/WeekList.jsx
@@ -20,19 +20,19 @@ function WeekList() {
       <div>
         <h2 className="subtitle is-5 has-text-centered">Your list of days and meals below:</h2>
         <br></br>
-        <h3 className="subtitle is-5 has-text-centered">Monday</h3>
+        <h3 className="title is-3 has-text-centered">Monday</h3>
         {mondayRecipe ? <RecipeCard recipe={mondayRecipe} /> : <Link to='/recipes'><button>Add Recipe For Monday</button></Link>}
-        <h3 className="subtitle is-5 has-text-centered">Tuesday</h3>
+        <h3 className="title is-3 has-text-centered">Tuesday</h3>
         {tuesdayRecipe ? <RecipeCard recipe={tuesdayRecipe} /> : <Link to='/recipes'><button>Add Recipe For Tuesday</button></Link>}
-        <h3 className="subtitle is-5 has-text-centered">Wednesday</h3>
+        <h3 className="title is-3 has-text-centered">Wednesday</h3>
         {wednesdayRecipe ? <RecipeCard recipe={wednesdayRecipe} /> : <Link to='/recipes'><button>Add Recipe For Tuesday</button></Link>}
-        <h3 className="subtitle is-5 has-text-centered">Thursday</h3>
+        <h3 className="title is-3 has-text-centered">Thursday</h3>
         {thursdayRecipe ? <RecipeCard recipe={thursdayRecipe} /> : <Link to='/recipes'><button>Add Recipe For Tuesday</button></Link>}
-        <h3 className="subtitle is-5 has-text-centered">Friday</h3>
+        <h3 className="title is-3 has-text-centered">Friday</h3>
         {fridayRecipe ? <RecipeCard recipe={fridayRecipe} /> : <Link to='/recipes'><button>Add Recipe For Tuesday</button></Link>}
-        <h3 className="subtitle is-5 has-text-centered">Saturday</h3>
+        <h3 className="title is-3 has-text-centered">Saturday</h3>
         {saturdayRecipe ? <RecipeCard recipe={saturdayRecipe} /> : <Link to='/recipes'><button>Add Recipe For Tuesday</button></Link>}
-        <h3 className="subtitle is-5 has-text-centered">Sunday</h3>
+        <h3 className="title is-3 has-text-centered">Sunday</h3>
         {sundayRecipe ? <RecipeCard recipe={sundayRecipe} /> : <Link to='/recipes'><button>Add Recipe For Tuesday</button></Link>}
       </div>
     </div>
