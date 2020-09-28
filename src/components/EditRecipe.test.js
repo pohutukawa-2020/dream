fd997fbde9c4a3efc1fc53e3ffdbbe751fbf0340
ariabjonesEDA <ariabjones@hotmail.com>
2020-09-28 16:11:23 +1300

    Trying to mock recipe props

--- src/components/EditRecipe.test.js
+++ src/components/EditRecipe.test.js
@@ -6,10 +6,11 @@ import { RecipeProvider } from './RecipeContext'
 
 
 it ('renders component correctly', () => {
-    const fakeParams = {params:'dlGn8GmbocFvxe8kJJ01'}
+    const fakeParams = { params: 'dlGn8GmbocFvxe8kJJ01' }
+    const fakeImage = {imagePath: 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1064518_11-651c583.jpg?quality=90&webp=true&resize=375,341'}
     const blah = renderer
         .create(<RecipeProvider>
-            <EditRecipe match={fakeParams}/>
+            <EditRecipe match={fakeParams} recipe={fakeImage}/>
             </RecipeProvider>)
         .toJSON()
     expect(blah).toMatchSnapshot()
