f2dc83bb9329e93daff8edb0994444533a641d17
Matt Silver <matt@Matts-MacBook-Pro.local>
2020-09-29 11:16:33 +1300

    WIP get recipe by user id working, breaking on weekplan assignment

--- src/components/RecipeContext.jsx
+++ src/components/RecipeContext.jsx
@@ -1,17 +1,24 @@
-import React, { useState, createContext, useEffect } from 'react'
+import React, { useState, createContext, useEffect, useContext } from 'react'
 
 import firebase from 'firebase/app'
+import 'firebase/auth'
+
+import { UserContext } from './UserContext'
 
 export const RecipeContext = createContext()
 
 export const RecipeProvider = ({ children }) => {
+  const {user} = useContext(UserContext)
   const [recipes, setRecipes] = useState([])
+  // const user = firebase.auth().currentUser
 
   useEffect(() => {
     const unsubscribe = firebase // note unsubscribe added in case funny behaviour
       .firestore()
       .collection('recipes')
+      .where('user', '==', user ? user.uid : null)
       .onSnapshot(snapshot => {
+        console.log(snapshot)
         const newRecipes = snapshot.docs.map(doc => ({
           id: doc.id,
           ...doc.data()
@@ -20,7 +27,7 @@ export const RecipeProvider = ({ children }) => {
       })
 
     return () => unsubscribe() // note unsubscribe added in case funny behaviour
-  }, [])
+  }, [user])
 
   return (
     <RecipeContext.Provider value={[recipes, setRecipes]}>
