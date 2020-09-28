24fb8f982bfe510e6e780c055564d2d4a24617ff
ariabjonesEDA <ariabjones@hotmail.com>
2020-09-28 18:45:42 +1300

    Clean out pseudo code

--- src/components/EditRecipe.test.js
+++ src/components/EditRecipe.test.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import renderer from 'react-test-renderer'
 
 import EditRecipe from './EditRecipe'
-import { RecipeProvider, RecipeContext } from './RecipeContext'
+import { RecipeContext } from './RecipeContext'
 
 export const FakeRecipeProvider = ({ children }) => {
     const [recipes, setRecipes] = useState([{
@@ -33,13 +33,3 @@ it ('renders component correctly', () => {
         .toJSON()
     expect(blah).toMatchSnapshot()
 })
-
-// {
-//     "id": "MOqgTKsRp9OwepdSupbr",
-//     "method": "[\"Preheat the oven to 200ºC/400ºF/gas 6. Peel 1 oni…]",
-//     "serves": "4",
-//     "prepTime": "40 Minutes - Not too tricky",
-//     "name": "Roasted black bean burgers",
-//     "ingredients": "[\"1½ red onions\", \"100 g rye bread\", \"ground corian…]",
-//     "imagePath": "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F5858755.jpg"
-//   },
