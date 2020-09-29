425733ebb42cb660e688cbec3de9072b5debc7a0
Matt Silver <matt@Matts-MacBook-Pro.local>
2020-09-29 23:43:46 +1300

    Add UserId to MiscItemContext

--- src/components/context/MiscShoppingContext.jsx
+++ src/components/context/MiscShoppingContext.jsx
@@ -1,16 +1,20 @@
-import React, { useState, createContext, useEffect } from 'react'
+import React, { useState, createContext, useEffect, useContext } from 'react'
 
 import firebase from 'firebase/app'
+
+import { UserContext } from './UserContext'
 
 export const MiscShoppingContext = createContext()
 
 export const MiscShoppingProvider = ({ children }) => {
+  const {user} = useContext(UserContext)
   const [miscCollection, setMiscCollection] = useState([])
 
   useEffect(() => {
     const unsubscribe = firebase // note unsubscribe added in case funny behaviour
       .firestore()
       .collection('miscShoppingList')
+      .where('user', '==', user ? user.uid : null)
       .onSnapshot(snapshot => {
         const miscCollection = snapshot.docs.map(doc => ({
           id: doc.id,
@@ -20,7 +24,7 @@ export const MiscShoppingProvider = ({ children }) => {
       })
 
     return () => unsubscribe() // note unsubscribe added in case funny behaviour
-  }, [])
+  }, [user])
 
   return (
     <MiscShoppingContext.Provider value={[miscCollection, setMiscCollection]}>
