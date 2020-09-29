aba0172d93330d33ca7575e01c2f87048cde532c
Matt Silver <matt@Matts-MacBook-Pro.local>
2020-09-29 23:20:53 +1300

    add hacky authenticated protected routes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,20 @@
 import React, { useContext, useState } from "react";
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 
 import { UserContext } from './context/UserContext'
 
 import { signIn, signInGoogle, signInFacebook, signOut } from '../utils'
 
 
-export default function Home() {
+export default function Home (props) {
   const {user} = useContext(UserContext)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const clickHandler = () => {
+    signIn(email, password)
+    props.history.push('/')
+  }
   
   return (
     <div>
@@ -19,7 +24,7 @@ export default function Home() {
         <label>Password</label><br></br>
         <input type='password' value={password} onChange={e => setPassword(e.target.value)}></input><br></br>
       </form>
-      <button onClick={() => signIn(email, password)}>Sign In</button>
+      <button onClick={() => clickHandler()}>Sign In</button>
       <Link to='/sign-up'><button>Register</button></Link><br></br>
       {/* <button onClick={signInGoogle}>Sign In With Google</button><br></br>
       <button onClick={signInFacebook}>Sign In With Facebook</button> */}
