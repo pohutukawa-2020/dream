53a69465acaf23f139552d6f52e8705d2c5c5302
Matt Silver <matt@Matts-MBP.fritz.box>
2020-10-06 21:41:34 +1300

    Update seed recipes to new ingredient naming convention

--- src/data/seedData.js
+++ src/data/seedData.js
@@ -4,14 +4,14 @@ const seedRecipes = [
     serves: 4,
     prepTime: 40,
     ingredients: [
-      {quantity: 1.5, name: 'red onions'},
-      {quantity: '100g', name: 'rye bread'},
-      {quantity: '1 400g can', name: 'black beans'},
-      {quantity: `40g`, name: 'cheddar cheese'},
-      {quantity: 4, name: 'soft rolls'},
-      {quantity: '100g', name: 'cherry tomatoes'},
-      {quantity: 1, name: 'mango'},
-      {quantity: 1, name: 'avocado'},
+      {quantity: 1.5, quantityType: '', name: 'red onions'},
+      {quantity: 100, quantityType: 'g', name: 'rye bread'},
+      {quantity: 1, quantityType: 'can', name: 'black beans'},
+      {quantity: 40, quantityType: 'g', name: 'cheddar cheese'},
+      {quantity: 4, quantityType: '', name: 'soft rolls'},
+      {quantity: 100, quantityType: 'g', name: 'cherry tomatoes'},
+      {quantity: 1, quantityType: '', name: 'mango'},
+      {quantity: 1, quantityType: '', name: 'avocado'},
     ],
     method: [
       'Preheat the oven to 200ºC/400ºF/gas 6. Peel 1 onion, place in a food processor with the mushrooms, rye bread and 1 teaspoon of ground coriander, and whiz until fine. Drain and pulse in the black beans, season lightly with sea salt and black pepper, then divide into 4 and shape into patties, roughly 2.5cm thick. Rub all over with oil and dust with ground coriander, then place on an oiled baking tray and roast for 25 minutes, or until dark and crispy, topping with the Cheddar and warming the rolls for the last few minutes.',
@@ -26,13 +26,13 @@ const seedRecipes = [
     serves: 6,
     prepTime: 50,
     ingredients: [
-      {quantity: '70g', name: 'butter'},
-      {quantity: '50g', name: 'plain flour'},
-      {quantity: '800ml', name: 'milk'},
-      {quantity: '800g', name: 'spinach'},
-      {quantity: '200g', name: 'ricotta cheese'},
-      {quantity: '300g', name: 'lasagne sheets'},
-      {quantity: '100g', name: 'parmesan cheese'}
+      {quantity: 70, quantityType: 'g', name: 'butter'},
+      {quantity: 50, quantityType: 'g', name: 'plain flour'},
+      {quantity: 800, quantityType: 'mL', name: 'milk'},
+      {quantity: 800, quantityType: 'g', name: 'spinach'},
+      {quantity: 200, quantityType: 'g', name: 'ricotta cheese'},
+      {quantity: 300, quantityType: 'g', name: 'lasagne sheets'},
+      {quantity: 100, quantityType: 'g', name: 'parmesan cheese'}
     ],
     method: [
       'Preheat the oven to 190°C/375°F/gas 5.',
@@ -49,18 +49,18 @@ const seedRecipes = [
     serves: 3,
     prepTime: 40,
     ingredients: [
-      {quantity: '400g', name: 'canned chickpeas'},
-      {quantity: '¼', name: 'red onion'},
-      {quantity: '1', name: 'carrot'},
-      {quantity: '1 clove', name: 'garlic'},
-      {quantity: '1', name: 'egg'},
-      {quantity: '1', name: 'lemon'},
-      {quantity: '2 tsp', name: 'ground cumin'},
-      {quantity: '1 tsp', name: 'ground corriander'},
-      {quantity: '1 pinch', name: 'ground cayenne pepper'},
-      {quantity: '1 tsp', name: 'baking powder'},
-      {quantity: '¼ cup', name: 'wholemeal breadcrumbs'},
-      {quantity: '1 cup', name: 'quinoa'}
+      {quantity: 400, quantityType: 'g', name: 'canned chickpeas'},
+      {quantity: 1/4, quantityType: '', name: 'red onion'},
+      {quantity: 1, quantityType: '', name: 'carrot'},
+      {quantity: 1, quantityType: '', name: 'clove garlic'},
+      {quantity: 1, quantityType: '', name: 'egg'},
+      {quantity: 1, quantityType: '', name: 'lemon'},
+      {quantity: 2, quantityType: 't', quantityType: '', name: 'ground cumin'},
+      {quantity: 1, quantityType: 't', name: 'ground corriander'},
+      {quantity: 1, quantityType: 'pinch', name: 'ground cayenne pepper'},
+      {quantity: 1, quantityType: 't', name: 'baking powder'},
+      {quantity: 1/4, quantityType: 'cup', name: 'wholemeal breadcrumbs'},
+      {quantity: 1, quantityType: 'cup', name: 'quinoa'}
     ],
     method: [
       'Put the quinoa on to cook and chop up some of your favourite salad ingredients.',
@@ -76,13 +76,13 @@ const seedRecipes = [
     serves: 3,
     prepTime: 90,
     ingredients: [
-      {quantity: '1.6 kilograms', name: 'chicken'},
-      {quantity: '1', name: 'lemon'},
-      {quantity: '2', name: 'red onions'},
-      {quantity: '2', name: 'carrots'},
-      {quantity: '1 tsp', name: 'ground cumin'},
-      {quantity: '200g', name: 'roasted red peppers, drained'},
-      {quantity: '250g', name: 'couscous'}
+      {quantity: 1.6, quantityType: 'kg', name: 'chicken'},
+      {quantity: 1, quantityType: '', name: 'lemon'},
+      {quantity: 2, quantityType: '', name: 'red onions'},
+      {quantity: 2, quantityType: '', name: 'carrots'},
+      {quantity: 1, quantityType: 't', name: 'ground cumin'},
+      {quantity: 200, quantityType: 'g', name: 'roasted red peppers, drained'},
+      {quantity: 250, quantityType: 'g', name: 'couscous'}
     ],
     method: [
       'It\'s best to use a nice free-range or organic chicken for this dish, or the best-quality bird you can afford; and I love the idea of the tasty vegetables in this recipe getting a whole lot of love. If you like, a mint sauce would make a great accompaniment. Pretty delish!', '2. Preheat the oven to 200°C. Place the chicken on a board and make some deep crisscross slashes into the legs – this will help the spices to penetrate the meat, and will also help it to cook more quickly. Halve the lemon and place inside the chicken, then rub the chicken skin all over with olive oil, a little sea salt, pepper and the cumin. Transfer the chicken to a roasting tray and place in the hot oven.',
@@ -97,16 +97,16 @@ const seedRecipes = [
     serves: 4,
     prepTime: 60,
     ingredients: [
-      {quantity: '2 cloves', name: 'garlic'},
-      {quantity: '5cm', name: 'ginger'},
-      {quantity: '1', name: 'onion'},
-      {quantity: '1', name: 'ripe tomato'},
-      {quantity: '1 tsp', name: 'cumin seeds'},
-      {quantity: '2 tsp', name: 'garam masala'},
-      {quantity: '1 tsp', name: 'turmeric'},
-      {quantity: '600g', name: 'frozen spinach'},
-      {quantity: '50 ml', name: 'cream'},
-      {quantity: '250g', name: 'paneer'},
+      {quantity: 2, quantityType: '', name: 'clove garlic'},
+      {quantity: 1, quantityType: 't', name: 'ginger'},
+      {quantity: 1, quantityType: '', name: 'onion'},
+      {quantity: 1, quantityType: '', name: 'ripe tomato'},
+      {quantity: 1, quantityType: 't', name: 'cumin seeds'},
+      {quantity: 2, quantityType: 't', name: 'garam masala'},
+      {quantity: 1, quantityType: 't', name: 'turmeric'},
+      {quantity: 600, quantityType: 'g', name: 'frozen spinach'},
+      {quantity: 50, quantityType: 'mL', name: 'cream'},
+      {quantity: 250, quantityType: 'g', name: 'paneer'},
     ],
     method: [
       'Drizzle a lug of oil into a large non-stick frying pan over a medium heat, add the paneer and fry for 5 minutes, or until golden, stirring frequently. Using a slotted spoon, transfer to a double layer of kitchen paper to drain.',
@@ -118,99 +118,6 @@ const seedRecipes = [
     ],
     imagePath: 'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/1443_1_1436884145.jpg?tr=w-800,h-1066https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/797_1_1436881994.jpg?tr=w-800,h-1066'
   }
-  // {
-  //   name: 'Roasted Root Vegetables',
-  //   serves: '12',
-  //   prepTime: '1 Hour cooking, 15 minutes prep - Not too tricky',
-  //   ingredients: [
-  //     '3 kg Maris Piper potatoes',
-  //     '16 carrots',
-  //     '12 parsnips',
-  //     '1 bulb of garlic',
-  //     '½ a bunch of fresh rosemary , (15g)'
-  //   ],
-  //   method: [
-  //     'Preheat the oven to 190°C/375°F/gas 5. Peel the potatoes, then scrub, top and tail the carrots and parsnips, cutting any larger ones in half lengthways. Cook in a large pan (or two) of boiling salted water for 8 minutes, then drain in a colander and leave to steam dry. Pick out the carrots and parsnips and put to one side, then give the colander a few light shakes to chuff up the potato edges.',
-  //     'Add 4 tablespoons of olive oil (or turkey fat) to two large roasting trays and season each with sea salt and black pepper. Squash the garlic bulb, then divide the cloves between the trays, along with the rosemary sprigs. Tip in the veg, add a good swig of red wine vinegar and toss to coat. Roast for 40 minutes, then remove from the oven and lightly squash with a fish slice to burst the skins. Place back in the oven for a further 20 minutes, or until golden and crisp.'
-  //   ],
-  //   imagePath: 'https://img.jamieoliver.com/jamieoliver/recipe-database/83911434.jpg?tr=w-800,h-1066'
-  // },
-  // {
-  //   name: 'Roast Chicken With Couscous',
-  //   serves: 'Aliens',
-  //   prepTime: '90 Minutes',
-  //   ingredients: [
-  //     '1.6 kilograms chicken ',
-  //     '1 lemon',
-  //     '2 red onions',
-  //     '2 carrots',
-  //     '3 sprigs fresh mint leaves',
-  //     'olive oil',
-  //     '1 teaspoon ground cumin',
-  //     '200 grams roasted red peppers, drained',
-  //     '1 teaspoon smoked paprika',
-  //     '1 tablespoon ground coriander',
-  //     '250 grams couscous',
-  //     'extra-virgin olive oil'
-  //   ],
-  //   method: [
-  //     '1. It’s best to use a nice free-range or organic chicken for this dish, or the best-quality bird you can afford; and I love the idea of the tasty vegetables in this recipe getting a whole lot of love. If you like, a mint sauce would make a great accompaniment. Pretty delish!', '2. Preheat the oven to 200°C. Place the chicken on a board and make some deep crisscross slashes into the legs – this will help the spices to penetrate the meat, and will also help it to cook more quickly. Halve the lemon and place inside the chicken, then rub the chicken skin all over with olive oil, a little sea salt, pepper and the cumin. Transfer the chicken to a roasting tray and place in the hot oven.',
-  //     '3. Peel and quarter the onions and carrots. When the chicken’s been cooking for around 15 minutes, reduce the heat to 180°C and add the onions and carrots to the tray. Roast for a further hour, or until golden and cooked through – the chicken is cooked when the thigh meat pulls easily away from the bone and the juices run clear.',
-  //     '4. Once perfectly cooked, carefully lift the chicken onto a plate or board and cover with tin foil and a couple of tea towels to keep warm. Spoon the vegetables onto a board and roughly chop them, then tip back into the tray and place on a medium heat. Pour in 500ml boiling water and stir well, making sure you scrape up all the lovely sticky goodness from the bottom of the tray.',
-  //     '5. Drain and roughly chop the peppers, then add them to the tray with the smoked paprika, ground coriander and the couscous. Pull the lemon halves out of the chicken with a pair of tongs and squeeze the juices into the tray (making sure to catch any pips). Bring to the boil, then turn the heat off and leave to rest for 5 minutes, or until the couscous has absorbed all of the water. Meanwhile, pick and finely chop the mint leaves. When the time’s up, fluff up the couscous using a fork and stir through the chopped mint. Drizzle with a good lug of extra virgin olive oil and everything together.', '6. Cut the chicken up into joints, removing the skin if you want to keep it healthy, then serve with the tasty roast-vegetable couscous. Enjoy!', '7. Nims Tip: Basting a chicken as it cooks helps to stop it drying out, giving you lovely, juicy meat. Check on the chicken every 20 to 30 minutes, and spoon any juices from the bottom of the tray back over the bird.'
-  //   ],
-  //   imagePath: 'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/797_1_1436881994.jpg?tr=w-800,h-1066'
-  // }
-  // {
-  //   name: 'Takeout Dinner Woohoo!',
-  //   serves: 'the whole family!',
-  //   prepTime: 'hopefully not too long!',
-  //   ingredients: [
-  //     'YAY no ingredients needed!'
-  //   ],
-  //   method: [
-  //     'Choose takeout',
-  //     'Order food',
-  //     'Enjoy!'
-  //   ],
-  //   imagePath: 'https://frontporchnewstexas.com/wp-content/uploads/2020/03/TAKE-OUT-TOGETHER-1024x768.png'
-  // },
-  // {
-  //   name: 'No recipe selected',
-  //   serves: 'select a recipe',
-  //   prepTime: 'select a recipe',
-  //   ingredients: [
-  //     'select a recipe'
-  //   ],
-  //   method: [
-  //     'select a recipe'
-  //   ],
-  //   imagePath: 'https://static.thenounproject.com/png/953211-200.png'
-  // },
-  // {
-  //   name: 'Out for dinner',
-  //   serves: 'the whole family!',
-  //   prepTime: 'hopefully not too long!',
-  //   ingredients: [
-  //     'YAY no ingredients needed!'
-  //   ],
-  //   method: [
-  //     'Get to the food!'
-  //   ],
-  //   imagePath: 'https://www.hadassah.org/regions/desert-mountain/chapters/lea/groups/dinner-out.jpg'
-  // },
-  // {
-  //   name: 'Leftovers for dinner',
-  //   serves: 'the whole family!',
-  //   prepTime: 'hopefully not too long!',
-  //   ingredients: [
-  //     'YAY no ingredients needed!'
-  //   ],
-  //   method: [
-  //     'Get to the food!'
-  //   ],
-  //   imagePath: 'https://lovefoodhatewaste.co.nz/wp-content/uploads/2016/10/Leftovers-1000.jpg'
-  // }
 ]
 
 export default seedRecipes
