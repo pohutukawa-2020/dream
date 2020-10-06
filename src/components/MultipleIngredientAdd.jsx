6838175fe2f4b22bb3d6651a2cf711c13646659c
Matt Silver <matt@Matts-MBP.fritz.box>
2020-10-06 21:41:01 +1300

    Update naming for MultipleIngredientAdd

--- src/components/MultipleIngredientAdd.jsx
+++ src/components/MultipleIngredientAdd.jsx
@@ -1,66 +1,81 @@
 
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { NewIngredientsContext } from './context/NewIngredientsContext'
 
 export default function MultipleFormTest () {
-  const [ingredients, setIngredients] = useState([{ quantity: null, quantityType: 't', name: '' }])
+  const [newIngredients, setNewIngredients] = useContext(NewIngredientsContext)
+  // const [ingredients, setIngredients] = useState([{ quantity: null, quantityType: 't', name: '' }])
   // const [ingredients, setIngredients] = useState([])
 
   // handle input change
   const handleInputChange = (evt, idx) => {
     const { name, value } = evt.target
-    const list = [...ingredients]
+    const list = [...newIngredients]
     list[idx][name] = value
-    setIngredients(list)
-    // setIngredients([...ingredients], { name, value })
+    setNewIngredients(list)
+    // setNewIngredients([...ingredients], { name, value })
   }
 
   // handle click event of the Remove button
   const handleRemoveClick = idx => {
-    const list = [...ingredients]
+    const list = [...newIngredients]
     list.splice(idx, 1)
-    setIngredients(list)
+    setNewIngredients(list)
   }
 
   // handle click event of the Add button
   const handleAddClick = () => {
-    setIngredients([...ingredients, { quantity: null, quantityType: 't', name: '' }])
+    setNewIngredients([...newIngredients, { quantity: null, quantityType: 't', name: '' }])
   }
 
   return (
-    <div className="newIngredientField">
-      {ingredients.map((x, idx) => {
+    <div>
+      {newIngredients.map((x, idx) => {
         return (
           <div>
+          <div className="newIngredientField">
             <input 
+              className="newIngredientQuantityField"
               name='quantity'
               type='number'
+              size="4"
+              maxlength='4'
               placeholder='Quantity'
               value={x.quantity}
               onChange={evt => handleInputChange(evt, idx)}
             />
-            <select name='quantityType' onChange={evt => handleInputChange(evt, idx)}>
-              <option value='teaspoon'>t</option>
-              <option value='tablespoon'>T</option>
+            <select className="newIngredientQuantityTypeField"
+              name='quantityType' onChange={evt => handleInputChange(evt, idx)}>
+              <option value='t'>t</option>
+              <option value='T'>T</option>
               <option value='mL'>mL</option>
-              <option value='litre'>Litre</option>
-              <option value='grams'>g</option>
-              <option value='kiloGrams'>kg</option>
-              <option value='can'>Can</option>
-              <option value='can'>Cup</option>
-              <option value='item'>item</option>
+              <option value='L'>L</option>
+              <option value='g'>g</option>
+              <option value='kg'>kg</option>
+              <option value='can'>can</option>
+              <option value='can'>cup</option>
+              <option value=''></option>
             </select>
             <input
+              className="newIngredientNameField"
               name="name"
 			        placeholder="Enter Ingredient"
               value={x.name}
               onChange={evt => handleInputChange(evt, idx)}
             />
-            <div>
-              {ingredients.length !== 1 && <button
-                onClick={() => handleRemoveClick(idx)}>Remove</button>}
-              {ingredients.length - 1 === idx && <button onClick={handleAddClick}>Add</button>}
+
+            <div className=
+            'newIngredientX'>
+              {newIngredients.length !== 1 && <h1
+                onClick={() => handleRemoveClick(idx)}>X</h1>}
             </div>
           </div>
+          <div className=
+          'newIngredientPlus'>
+            {newIngredients.length - 1 === idx && <button onClick={handleAddClick}>Add another ingredient</button>}
+          </div>
+          </div>
+          
         )
       })}
     </div>
